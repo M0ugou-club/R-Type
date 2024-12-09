6bf2b27fbf0f30178045be7f0614e4183f2e9891
antonincmp <antonin.campi@epitech.eu>
2024-12-09 10:08:05 +0100

    ADD: better collision system

--- src/graphical/Game.hpp
+++ src/graphical/Game.hpp
@@ -15,6 +15,7 @@
 #include "../ecs/SparseArray.hpp"
 #include "../ecs/EntitiesGestion.hpp"
 
+
 void enemy_system(Registry &registry, Entities &enemy) {
     if (registry.get_components<Position>()[enemy]->y < 0) {
         registry.get_components<Velocity>()[enemy]->y = 100;
@@ -23,35 +24,21 @@ void enemy_system(Registry &registry, Entities &enemy) {
     }
 }
 
-void collision_system(Registry &registry) {
+void collision_system(Registry &registry, Entities &enemy) {
     auto &positions = registry.get_components<Position>();
     auto &drawables = registry.get_components<Draw>();
+    auto &controls = registry.get_components<Control>();
 
     for (std::size_t i = 0; i < positions.size(); ++i) {
-        if (!positions[i] || !drawables[i])
-            continue;
-        Draw *enemyDraw = drawables[i].operator->();
-        if (enemyDraw->color.r == 255 && enemyDraw->color.g == 0 && enemyDraw->color.b == 0) {
-            float ex = positions[i]->x;
-            float ey = positions[i]->y;
-            float ew = (float)enemyDraw->rect.w;
-            float eh = (float)enemyDraw->rect.h;
-
-            for (std::size_t j = 0; j < positions.size(); ++j) {
-                if (!positions[j] || !drawables[j])
-                    continue;
-                Draw *projDraw = drawables[j].operator->();
-                if (projDraw->color.r == 0 && projDraw->color.g == 0 && projDraw->color.b == 255) {
-                    float px = positions[j]->x;
-                    float py = positions[j]->y;
-                    float pw = (float)projDraw->rect.w;
-                    float ph = (float)projDraw->rect.h;
-
-                    bool collision = (px < ex + ew && px + pw > ex && py < ey + eh && py + ph > ey);
-                    if (collision) {
-                        std::cout << "Touché" << std::endl;
-                    }
-                }
+        if (positions[i] && drawables[i]) {
+            if (i == enemy) {
+                continue;
+            }
+            if (positions[i]->x < positions[enemy]->x + drawables[enemy]->rect.w &&
+                positions[i]->x + drawables[i]->rect.w > positions[enemy]->x &&
+                positions[i]->y < positions[enemy]->y + drawables[enemy]->rect.h &&
+                positions[i]->y + drawables[i]->rect.h > positions[enemy]->y) {
+                std::cout << "Touché" << std::endl;
             }
         }
     }
@@ -63,7 +50,7 @@ void handle_shoot(Registry &registry, Entities &entity) {
 
     auto projectile = registry.spawn_entity();
     registry.add_component<Position>(projectile, Position(positions[entity]->x + 50, positions[entity]->y + 20));
-    registry.add_component<Velocity>(projectile, Velocity(200, 0));
+    registry.add_component<Velocity>(projectile, Velocity(400, 0));
     registry.add_component<Draw>(projectile, Draw({0, 0, 255, 255}, {10, 10, 10, 10}));
 
 }
