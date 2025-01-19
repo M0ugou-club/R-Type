809e7c28c3c12f192565915550e04517b237730e
Xer0ce <Xer0ce@users.noreply.github.com>
2025-01-19 00:32:25 +0000

    deploy: 858fe781abd76a867a5ddf6f166b2aaf8d0b9529

--- doxygen/html/search/all_9.js
+++ doxygen/html/search/all_9.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['lobby_0',['Lobby',['../classLobby.html',1,'']]]
+  ['lobby_0',['Lobby',['../classLobby.html',1,'']]],
+  ['lobbyhistory_1',['LobbyHistory',['../classLobbyHistory.html',1,'']]]
 ];
